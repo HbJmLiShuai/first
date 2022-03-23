48ad1c685fb550f631e756e784f089b22de3b4f1
ls <1242945164@qq.com>
2022-03-23 17:17:56 +0800

    sql脚本

---
+++ qianduan/sql脚本/denglu.sql
@@ -0,0 +1,7 @@
+INSERT INTO `denglu` VALUES (1, 'ls', 'ls');
+INSERT INTO `denglu` VALUES (2, 'lxy', 'als');
+INSERT INTO `denglu` VALUES (4, 'lslxy', '1314');
+INSERT INTO `denglu` VALUES (6, 'ming', 'ming');
+INSERT INTO `denglu` VALUES (100, 'l', 's');
+INSERT INTO `denglu` VALUES (110, 'hh', 'bi');
+INSERT INTO `denglu` VALUES (127, 'fw', 'fw');
